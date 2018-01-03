4d37aee64b78a9e777d5f47dc9da8c4445aeea47
randsom <sandoz.romain@gmail.com>
2018-01-03 15:11:50 +0100

    Initialize Sequelize workflow.

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
 var express = require('express');
-const router = express.Router();
-
+var router = express.Router();
+var pg = require('pg');
 
 // define the home page route
 router.get('/', function(req, res, next) {
-    console.log("get all users");
-    res.send("get all users"); //Should ask the DB and return list of users
+
 });
-// define the about route
+
+
+
 router.get('/:id', function(req, res, next) {
     console.log(req.params.id);
     res.send(req.params.id);
